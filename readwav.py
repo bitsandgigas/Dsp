dcaefe565c36c4ca1115c24a90619e0d69f62c4d
bitsandgigas <52226234+bitsandgigas@users.noreply.github.com>
2025-04-05 11:28:33 -0400

    Update readwav.py
    add max voltage variable

--- readwav.py
+++ readwav.py
@@ -2,10 +2,10 @@ from scipy.io import wavfile
 
 def read_wav_file(file_path):
     # Read the sample rate and audio data
-    sample_rate, audio_data = wavfile.read(file_path)
+    sample_rate, audio_data = wavfile.read(file_path,maxV)
     
     # Create the time series
     time_series = [i / sample_rate for i in range(len(audio_data))]
-    
+    audio_data = audio_data*maxV;
     return time_series, sample_rate, audio_data
 
